a38eac95e9e05805b9ca98755e44a240fb80c91e
Md. Nazmus Sakib <96543338+engrsakib@users.noreply.github.com>
2024-03-02 23:14:33 +0600

    Update check_bst.cpp

--- print level order inorder/check_bst.cpp
+++ print level order inorder/check_bst.cpp
@@ -14,84 +14,54 @@ public:
         this->right = NULL;
     }
 };
-// Input
-Node *input_Node()
+vector<int> vv;
+Node *convert(vector<int> v, int n, int l, int r)
 {
-    int val;
-    cin >> val;
-    Node *root;
-    if (val == -1)
-        root = NULL;
-    else
-        root = new Node(val);
-
-    queue<Node *> q;
-    if (root)
-        q.push(root);
-    while (!q.empty())
-    {
-        Node *p = q.front();
-        q.pop();
-        // Kaj Jeta amora korbo
-        int l, r;
-        cin >> l >> r;
-        Node *my_Left;
-        Node *my_Right;
-        if (l == -1)
-            my_Left = NULL;
-        else
-            my_Left = new Node(l);
-        if (r == -1)
-            my_Right = NULL;
-        else
-            my_Right = new Node(r);
-
-        // Cannection
-        p->left = my_Left;
-        p->right = my_Right;
-
-        // Chield PUSH
-        if (p->left)
-            q.push(p->left);
-        if (p->right)
-            q.push(p->right);
-    }
+    if (l > r)
+        return NULL;
+    int mid = (l + r) / 2;
+    Node *root = new Node(v[mid]);
+    Node *left_Root = convert(v, n, l, mid - 1);
+    Node *right_Root = convert(v, n, mid + 1, r);
+    root->left = left_Root;
+    root->right = right_Root;
     return root;
 }
-
 // Check if the tree is a binary search tree (BST)
-bool is_BST_Util(Node *root, int &prev)
-{
-    if (root == NULL)
-        return false;
-
-    if (!is_BST_Util(root->left, prev))
-        return true;
-
-    if (root->val <= prev)
-        return true;
-
-    prev = root->val;
-
-    return is_BST_Util(root->right, prev);
-}
 // Check BST is or Not
 bool is_BST(Node *root)
 {
-    int prev = INT_MIN;
-    return is_BST_Util(root, prev);
+    for (int i = 0; i < vv.size(); i++)
+    {
+        for (int j = i + 1; j < vv.size() - 1; j++)
+        {
+            if (vv[i] > vv[j])
+                return false;
+        }
+    }
+    return true;
 }
 int main()
 {
     // Md. Nazmus Sakib
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    Node *root = input_Node();
+    vector<int> v;
+    int data;
+
+    while (cin >> data)
+    {
+        v.emplace_back(data);
+    }
+    // sort(v.begin(), v.end());
+    Node *root = convert(v, v.size(), 0, v.size() - 1);
 
     if (is_BST(root))
         cout << endl
-             << "This is a Binary Tree" << endl;
+             << endl
+             << "This is a Binary Search Tree" << endl;
     else
         cout << endl
-             << "This is not a Binary Tree" << endl;
+             << endl
+             << "This is not a Binary Search Tree" << endl;
     return 0;
 }
