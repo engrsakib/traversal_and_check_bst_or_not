36bde62eb9d64790c88164bd213a7a6577f82a86
Md. Nazmus Sakib <96543338+engrsakib@users.noreply.github.com>
2024-03-02 23:15:18 +0600

    Update print_Node.cpp

--- print level order inorder/print_Node.cpp
+++ print level order inorder/print_Node.cpp
@@ -1,87 +1,78 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class Node{
-    public:
-        int val;
-        Node *left;
-        Node *right;
-        Node(int val)
-        {
-            this->val = val;
-            this->left = NULL;
-            this->right = NULL;
-        }
-};
-//Input
-Node *input_Node()
+class Node
 {
+public:
     int val;
-    cin >> val;
-    Node *root;
-    if(val == -1) root = NULL;
-    else root = new Node(val);
-
-    queue <Node *> q;
-    if(root) q.push(root);
-    while(!q.empty())
+    Node *left;
+    Node *right;
+    Node(int val)
     {
-        Node *p = q.front();
-        q.pop();
-        //Kaj Jeta amora korbo
-        int l, r;
-        cin >> l >> r;
-        Node *my_Left;
-        Node *my_Right;
-        if(l == -1) my_Left = NULL;
-        else my_Left = new Node(l);
-        if(r == -1) my_Right = NULL;
-        else my_Right = new Node(r);
-
-        // Cannection
-        p->left = my_Left;
-        p->right = my_Right;
-
-        //Chield PUSH
-        if(p->left) q.push(p->left);
-        if(p->right) q.push(p->right);
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
     }
+};
+Node *convert(vector<int> v, int n, int l, int r)
+{
+    if (l > r)
+        return NULL;
+    int mid = (l + r) / 2;
+    Node *root = new Node(v[mid]);
+    Node *left_Root = convert(v, n, l, mid - 1);
+    Node *right_Root = convert(v, n, mid + 1, r);
+    root->left = left_Root;
+    root->right = right_Root;
     return root;
 }
-//in order
+// in order
 void in_Order(Node *root)
 {
-    if (root == NULL) return;
+    if (root == NULL)
+        return;
     in_Order(root->left);
     cout << root->val << " ";
     in_Order(root->right);
 }
-//pre Oreder
+// pre Oreder
 void pre_order(Node *root)
 {
-    if(root == NULL) return;
+    if (root == NULL)
+        return;
     cout << root->val << " ";
     pre_order(root->left);
     pre_order(root->right);
 }
-//Post Order
+// Post Order
 void post_Order(Node *root)
 {
-    if(root == NULL) return;
+    if (root == NULL)
+        return;
     post_Order(root->left);
     post_Order(root->right);
     cout << root->val << " ";
 }
-int main() 
+int main()
 {
-    //Md. Nazmus Sakib
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    Node *root = input_Node();
+    // Md. Nazmus Sakib
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> v;
+    int data;
+
+    while (cin >> data)
+    {
+        v.emplace_back(data);
+    }
+    sort(v.begin(), v.end());
+    Node *root = convert(v, v.size(), 0, v.size() - 1);
     cout << "print Pre_Order: " << endl;
     pre_order(root);
-    cout << endl << "Print In_Order: " << endl;
+    cout << endl
+         << "Print In_Order: " << endl;
     in_Order(root);
-    cout << endl << "Print Post_Order: " << endl;
-    post_Order(root); 
+    cout << endl
+         << "Print Post_Order: " << endl;
+    post_Order(root);
     return 0;
 }
